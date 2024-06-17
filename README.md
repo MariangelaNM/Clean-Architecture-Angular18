016951bd50005aa55ef396801b3061789b43cad7
MariangelaNM <91926755+MariangelaNM@users.noreply.github.com>
2024-06-17 13:34:21 -0600

    Add template

---
+++ README.md
@@ -0,0 +1,6 @@
+Explicacion de la arquitectura
+ - https://medium.com/bancolombia-tech/arquitectura-limpia-en-angular-17-25c129ed2f7a
+
+Ejecutar el proyecto
+    - npm i
+    - npm start
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/presentacion/pages/app/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule, {
+  ngZoneEventCoalescing: true
+})
+  .catch(err => console.error(err));
